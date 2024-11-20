3e934cddfe1d80cc3472e8771c0065026f1c2526
OlegPotapov <olezhka_potapov_1995@bk.ru>
2024-11-20 23:30:20 +0300

    удалил прошлую бд, сделал файл database на основе fastapi_users, пока что только модели подправил и создал таблицы в новой бд

--- my_app_new/models.py
+++ my_app_new/models.py
@@ -1,25 +1,33 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
-from my_app_new.database import Base
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from my_app_new.auth.database import Base
 
 
 class User(Base):
-    __tablename__ = "users"
+    __table_args__ = {'extend_existing': True}
+    __tablename__ = "user"
 
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
 
+    is_superuser: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
+    is_verified: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
+
     items = relationship("Item", back_populates="owner")
 
 
 class Item(Base):
-    __tablename__ = "items"
+    __tablename__ = "item"
 
     id = Column(Integer, primary_key=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner_id = Column(Integer, ForeignKey('user.id'))
 
     owner = relationship("User", back_populates="items")
