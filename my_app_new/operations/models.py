63519e2461df7e2cf6cfae2ae43bf3aa997a131d
OlegPotapov <olezhka_potapov_1995@bk.ru>
2024-11-25 15:34:15 +0300

    с прошлого комита ничего не прокатило. закоментил все свои схемы, модели, удалил бд, сделал все заново для аутентификации. сейчас в папке ауф все работает. осталось создать логику приложения не зависомо от моделей ауф. надо скоректировать модели, миграции, схемы, крид и тд... независомо от таблицы юзер для аутентификации.

---
+++ my_app_new/operations/models.py
@@ -0,0 +1,35 @@
+# from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+# from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, MetaData
+# from sqlalchemy.orm import relationship, Mapped, mapped_column
+# from my_app_new.auth.database import Base
+#
+# metadata = MetaData()
+
+# class User(Base):
+#     __table_args__ = {'extend_existing': True}
+#     __tablename__ = "user"
+#
+#     id = Column(Integer, primary_key=True)
+#     email = Column(String, unique=True, index=True)
+#     hashed_password = Column(String)
+#     is_active = Column(Boolean, default=True)
+#
+#     is_superuser: Mapped[bool] = mapped_column(
+#         Boolean, default=False, nullable=False
+#     )
+#     is_verified: Mapped[bool] = mapped_column(
+#         Boolean, default=False, nullable=False
+#     )
+#
+#     items = relationship("Item", back_populates="owner")
+#
+#
+# class Item(Base):
+#     __tablename__ = "item"
+#
+#     id = Column(Integer, primary_key=True)
+#     title = Column(String, index=True)
+#     description = Column(String, index=True)
+#     owner_id = Column(Integer, ForeignKey('user.id'))
+#
+#     owner = relationship("User", back_populates="items")
