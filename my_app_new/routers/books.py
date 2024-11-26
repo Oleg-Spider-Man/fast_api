16aed6ce1f7a005b693b6388c249cc1fc6e4e13f
OlegPotapov <olezhka_potapov_1995@bk.ru>
2024-11-26 17:15:57 +0300

    сделал новые таблицы автор и книги, наладил и сделал миграции, создал таблицы в бд. Начал налаживать папку operations. В ней сделал роутер на книги(только схемы не сделал), для книг наладил крид. Начал налаживать роутер Авторы. Пока что только на чтение Авторов и Автора (но без схем).

--- my_app_new/routers/books.py
+++ my_app_new/routers/books.py
@@ -5,12 +5,12 @@ from my_app_new.dependencies import get_db
 from sqlalchemy.orm import Session
 
 router = APIRouter(
-    prefix="/items",
-    tags=["items"],
+    prefix="/books",
+    tags=["books"],
 )
 
 
 @router.get("/", response_model=list[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
+def read_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    books = crud.get_books(db, skip=skip, limit=limit)
+    return books
