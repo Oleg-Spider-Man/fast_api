5bf768adaf1be9026739d5427c50e52fdcaeded4
OlegPotapov <olezhka_potapov_1995@bk.ru>
2024-11-29 18:23:44 +0300

    осталось доделать два эндпоинта для авторов и их криды. После ограничение поставить от куреентюзер. Здесь поправил методы алхимии на асинхронные.

--- my_app_new/operations/crud.py
+++ my_app_new/operations/crud.py
@@ -1,42 +1,42 @@
 
 from sqlalchemy.ext.asyncio import AsyncSession
-# from sqlalchemy.orm import Session
 from sqlalchemy import select
 from my_app_new.operations import models, schemas
 
 #
-# def get_author(db: Session, author_id: int):
+# def get_author(db: AsyncSession, author_id: int):
 #     return db.query(models.Author).filter(models.Author.id == author_id).first()
-#
-#
-# def get_author_by_author_name(db: Session, author_name: str):
-#     return db.query(models.Author).filter(models.Author.author_name == author_name).first()
-#
-#
-# def get_authors(db: Session, skip: int = 0, limit: int = 100):
-#     return db.query(models.Author).offset(skip).limit(limit).all()
-#
-#
-# def create_author(db: Session, author: schemas.AuthorCreate):
-#
-#     db_author = models.Author(author_name=author.author_name)
-#     db.add(db_author)
-#     db.commit()
-#     db.refresh(db_author)
-#     return db_author
-#
+
+
+async def get_author_by_author_name(db: AsyncSession, author_name: str):
+    query = select(models.Author).filter(models.Author.author_name == author_name)
+    result = await db.execute(query)
+    return result.scalar()
+# return db.query(models.Author).filter(models.Author.author_name == author_name).first() было так
+
+
+async def get_authors(db: AsyncSession, skip: int = 0, limit: int = 100):
+    query = select(models.Author).offset(skip).limit(limit)
+    result = await db.execute(query)
+    return result.scalars().all()
+
+
+async def create_author(db: AsyncSession, author: schemas.AuthorCreate):
+
+    db_author = models.Author(author_name=author.author_name)
+    db.add(db_author)
+    await db.commit()
+    return db_author
+
 
 async def get_books(db: AsyncSession, skip: int = 0, limit: int = 100):
-    #return db.select(models.Book).offset(skip).limit(limit).all()
     query = select(models.Book).offset(skip).limit(limit)
     result = await db.execute(query)
     return result.scalars().all()
-    # a = result.scalars().all()
-    # a = list(a)
-    # return a
+
 
 #
-# def create_book_author(db: Session, book: schemas.BookCreate, author_id: int):
+# def create_book_author(db: AsyncSession, book: schemas.BookCreate, author_id: int):
 #     db_book = models.Book(**book.dict(), author_id=author_id)
 #     db.add(db_book)
 #     db.commit()
