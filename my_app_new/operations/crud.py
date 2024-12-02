3fac15c28811c1fb5a8496c65b04882f0d26544d
OlegPotapov <olezhka_potapov_1995@bk.ru>
2024-12-02 15:18:20 +0300

    вроде все перед тестами

--- my_app_new/operations/crud.py
+++ my_app_new/operations/crud.py
@@ -1,18 +1,18 @@
-
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from my_app_new.operations import models, schemas
 
-#
-# def get_author(db: AsyncSession, author_id: int):
-#     return db.query(models.Author).filter(models.Author.id == author_id).first()
+
+async def get_author(db: AsyncSession, author_id: int):
+    query = select(models.Author).filter(models.Author.id == author_id)
+    result = await db.execute(query)
+    return result.scalar()
 
 
 async def get_author_by_author_name(db: AsyncSession, author_name: str):
     query = select(models.Author).filter(models.Author.author_name == author_name)
     result = await db.execute(query)
     return result.scalar()
-# return db.query(models.Author).filter(models.Author.author_name == author_name).first() было так
 
 
 async def get_authors(db: AsyncSession, skip: int = 0, limit: int = 100):
@@ -35,10 +35,8 @@ async def get_books(db: AsyncSession, skip: int = 0, limit: int = 100):
     return result.scalars().all()
 
 
-#
-# def create_book_author(db: AsyncSession, book: schemas.BookCreate, author_id: int):
-#     db_book = models.Book(**book.dict(), author_id=author_id)
-#     db.add(db_book)
-#     db.commit()
-#     db.refresh(db_book)
-#     return db_book
+async def create_book_author(db: AsyncSession, book: schemas.BookCreate, author_id: int):
+    db_book = models.Book(**book.dict(), author_id=author_id)
+    db.add(db_book)
+    await db.commit()
+    return db_book
