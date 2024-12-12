9c37e69add55e09c84c94ec756047f95177433ff
OlegPotapov <olezhka_potapov_1995@bk.ru>
2024-12-12 16:13:58 +0300

    долго парился с асинхронными тестами в pytest из-за проблем event loop. Сделал scop=func вместо session так как - [200~Pytest-asyncio v0.24 поддерживает запуск тестов в различных циклах событий.

--- my_app_new/dependencies.py
+++ my_app_new/dependencies.py
@@ -5,15 +5,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from my_app_new.database import async_session_maker
 
 
-# !!!!!!!!!!!! ПОМЕНЯТЬ ЭТУ СЕССИЮ НА СЕССИЮ ИЗ ДАТАБЕЙС
-# def get_db():
-#     db = async_session_maker()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
