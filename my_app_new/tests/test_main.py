f41657ffa75f94df0d6701741735e31416d8084b
OlegPotapov <olezhka_potapov_1995@bk.ru>
2024-12-16 16:55:31 +0300

    сделал тесты для авторов и книг, авторизации юзера и сейчас на аутентификации юзера пытаюсь получить токен из кук в глобальную переменную для след тестов(пока что ограничение на тестах автора и книг убрал, потом вернуть.

--- my_app_new/tests/test_main.py
+++ my_app_new/tests/test_main.py
@@ -1,7 +1,4 @@
 import pytest
-from fastapi.testclient import TestClient
-from my_app_new.main import app
-client = TestClient(app)
 
 
 @pytest.mark.asyncio
@@ -13,7 +10,45 @@ async def test_create_author(aclient):
 
 
 @pytest.mark.asyncio
+async def test_read_authors(aclient):
+    response = await aclient.get("/authors/")
+    assert response.status_code == 200
+    assert response.json() == [{"author_name": "Jane Doe", "id": 1}]
+
+
+@pytest.mark.asyncio
+async def test_read_author(aclient):
+    response = await aclient.get("/authors/1")
+    assert response.status_code == 200
+    assert response.json() == {"author_name": "Jane Doe", "id": 1}
+
+
+@pytest.mark.asyncio
+async def test_create_book_for_author(aclient):
+    response = await aclient.post("/authors/1/books/", json={
+     "title": "Богатство",
+     "description": "очень интересные события",
+     "genre": "роман-эпопея",
+     "price": 177
+    })
+    assert response.status_code == 200
+    assert response.json() == {
+     "title": "Богатство",
+     "description": "очень интересные события",
+     "genre": "роман-эпопея",
+     "price": 177,
+     "id": 1
+    }
+
+
+@pytest.mark.asyncio
 async def test_read_books(aclient):
     response = await aclient.get('/books/')
     assert response.status_code == 200
-
+    assert response.json() == [{
+     "title": "Богатство",
+     "description": "очень интересные события",
+     "genre": "роман-эпопея",
+     "price": 177,
+     "id": 1
+    }]
