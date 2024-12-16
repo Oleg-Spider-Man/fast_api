24825819a8a3c891644c63baec101a868e448fc2
OlegPotapov <olezhka_potapov_1995@bk.ru>
2024-12-16 16:55:31 +0300

    сделал тесты для авторов и книг, авторизации юзера и сейчас на аутентификации юзера пытаюсь получить токен из кук в глобальную переменную для след тестов(пока что ограничение на тестах автора и книг убрал, потом вернуть.

--- my_app_new/tests/conftest.py
+++ my_app_new/tests/conftest.py
@@ -1,6 +1,7 @@
 import asyncio
 import httpx
 import pytest
+from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from my_app_new.config import DB_USER, DB_HOST, DB_PORT, DB_NAME_TEST, DB_PASS
 from my_app_new.database import Base
@@ -10,9 +11,9 @@ from my_app_new.main import app
 
 DATABASE_URL_TEST = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME_TEST}"
 
-engine_test = create_async_engine(DATABASE_URL_TEST, echo=True)
-
-TestSessionLocal = async_sessionmaker(bind=engine_test, class_=AsyncSession, expire_on_commit=False)#sessionmaker(bind=engine_test, class_=AsyncSession, expire_on_commit=False)
+engine_test = create_async_engine(DATABASE_URL_TEST, poolclass=pool.NullPool, echo=True)
+# NullPool для тестов можно, для продакшена нельзя.
+TestSessionLocal = async_sessionmaker(bind=engine_test, class_=AsyncSession, expire_on_commit=False)
 
 metadata = Base.metadata
 
@@ -26,20 +27,14 @@ async def override_get_async_session():
 app.dependency_overrides[get_async_session] = override_get_async_session
 
 
-@pytest.fixture(scope='session')  # ,  autouse=True)
+@pytest.fixture(scope='session')
 def event_loop(request):
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
 
-# @n-ellocoPytest-asyncio v0.24 поддерживает запуск тестов в различных циклах событий.
-# Для каждого уровня набора тестов (сеанс, пакет, модуль, класс, функция) есть цикл.
-# Однако pytest-asyncio ошибочно предполагает, что область действия асинхронной фикстуры совпадает с областью
-# действия цикла событий, в котором выполняется фикстура. Это по сути делает невозможным наличие фикстуры
-# с областью действия сеанса, которая выполняется в том же цикле, что и тест с областью действия функции. См. #706 .
 
-
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 async def db_session():
     async with engine_test.begin() as conn:
         await conn.run_sync(metadata.create_all)
@@ -48,7 +43,7 @@ async def db_session():
         await conn.run_sync(metadata.drop_all)
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 async def aclient():
     async with httpx.AsyncClient(app=app, base_url="http://test") as aclient:
         yield aclient
