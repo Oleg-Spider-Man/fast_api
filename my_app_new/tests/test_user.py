b3534ed539ec64c9cd81afc725e356c806b773f1
OlegPotapov <olezhka_potapov_1995@bk.ru>
2024-12-19 16:46:04 +0300

    сделал первыми тест на регистрацию и аутентификацию. После получил токен и сохранил его в куки в клиенте - aclient.cookies.set('имя куки из фастапи приложения, полученный токен'. Еще тест на выход из ау не сделан

--- my_app_new/tests/test_user.py
+++ my_app_new/tests/test_user.py
@@ -1,42 +1,5 @@
-import pytest
+# import pytest
 
 
-@pytest.mark.asyncio
-async def test_register(aclient):
-    response = await aclient.post("/auth/register", json={
-        "email": "user@example.com",
-        "password": "string",
-        "is_active": True,
-        "is_superuser": False,
-        "is_verified": False
-     })
-    assert response.status_code == 201
-    assert response.json() == {
-        "id": 1,
-        "email": "user@example.com",
-        "is_active": True,
-        "is_superuser": False,
-        "is_verified": False
-    }
-
-
-@pytest.mark.asyncio
-async def test_get_auth(aclient):
-    login_data = {
-        "username": "user@example.com",
-        "password": "string"
-    }
-    response = await aclient.post("/auth/jwt/login", data=login_data)
-    assert response.status_code == 204 # 200
-
-    # Получаем куки из ответа
-    cookies = response.cookies
-
-    # Проверяем наличие токена в куки
-    token_cookie = cookies.get('cookie_fast_api_new')
-    assert token_cookie is not None
-    # разобраться как куки получить в глобальную переменную и использовать в других тестах
-    global_access_token = token_cookie
-    pytest.global_vars['access_token'] = global_access_token
 
 
