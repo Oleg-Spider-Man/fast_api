516af48799ac4cfbcea2df84c94b936b5f99fc0a
OlegPotapov <olezhka_potapov_1995@bk.ru>
2025-01-09 08:27:05 +0300

    докеркомпоз заработал но теперь проблема с получением jwt из кук браузера. Потому что докер контейнер это изолированная среда.

--- docker/celery.sh
+++ docker/celery.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-cd my_app_new
-
-if [["${1}" == "celery"]]; then
-  celery --app=background_tasks.celery_task:celery_app worker -l INFO
-elif [["${1}" == "flower"]]; then
-  celery --app=background_tasks.celery_task:celery_app flower
- fi
-
+celery --app=my_app_new.background_tasks.celery_task:celery_app worker -l INFO
 
