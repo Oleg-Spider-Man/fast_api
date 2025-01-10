c8dbc9bee974d4ce4c35b2b6963e1ccb17a4924e
OlegPotapov <olezhka_potapov_1995@bk.ru>
2025-01-11 00:24:12 +0300

    что бы куки отправлялись добавил аргумент secure=false в описании кук в фастапиюзер и в мэин добавил настройки мидлвер и корс важные для кук. ТЕПЕРЬ ВСЕ РАБОТАЕТ!

--- my_app_new/auth_/auth.py
+++ my_app_new/auth_/auth.py
@@ -3,7 +3,7 @@ from fastapi_users.authentication import JWTStrategy
 
 from my_app_new.config import SECRET_JWT
 
-cookie_transport = CookieTransport(cookie_name='cookie_fast_api_new', cookie_max_age=3600)
+cookie_transport = CookieTransport(cookie_name='cookie_fast_api_new', cookie_max_age=3600, cookie_secure=False)
 
 
 def get_jwt_strategy() -> JWTStrategy:
